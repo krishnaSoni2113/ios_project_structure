3f42fbe90d3f61ec9117e2608c097b46510049bb
KRISHNA SONI <mac-0005@example.com>
2019-09-06 15:43:16 +0530

    - Updated network file

--- ProjectStructure/Services/Networking.swift
+++ ProjectStructure/Services/Networking.swift
@@ -11,8 +11,8 @@ import Alamofire
 
 var APIURL:String        =   "http://192.168.1.162/sevenchats_mul/backend/api/v1/"
 
-//MARK:- ---------Networking
-//MARK:-
+//MARK:- ---------Networking -
+
 typealias ClosureSuccess = (_ task:URLSessionTask, _ response:AnyObject?, _ data: Data?) -> Void
 typealias ClosureError   = (_ task:URLSessionTask, _ message:String?, _ error:NSError?) -> Void
 
@@ -36,6 +36,9 @@ class Networking: NSObject {
         return ["Authorization" : "Bearer 123456789","Content-Type" : "application/json", "Accept-Language" : "en", "language":"1","Accept" : "application/json"]
     }
     
+    private let arrSuccessStatus: [Int] = [200, 201, 401]
+    
+    
     var loggingEnabled = true
     var activityCount = 0
     var backroundSession = URLSessionConfiguration.background(withIdentifier: "com.socialmedia.app.backgroundtransfer")
@@ -53,78 +56,166 @@ class Networking: NSObject {
 }
 
 
-//MARK:- --------- Api Methods
-//MARK:-
+//MARK:- --------- Api Methods -
+
 extension Networking {
     
-    // Api Service with tag
-    func apiService(apiTag tag:String, param parameters: [String: AnyObject]?,apiMethod method: HTTPMethod, success : ClosureSuccess?, failure:ClosureError?) -> URLSessionTask? {
-        let uRequest = SessionManager.default.request((BASEURL + tag), method: method, parameters: parameters, encoding: URLEncoding.default, headers: headers)
-        self.handleResponseStatus(uRequest: uRequest, success: success, failure: failure)
+    /*
+     // The way to pass the additional data in header. Use this before calling the api from service file.
+     
+     var header = Networking.sharedInstance.headers
+     header["name"] = "krishna"
+     */
+    
+    // Api Service
+    func apiService(
+        apiTag tag: String? = nil,
+        param parameters: [String: AnyObject]?,
+        apiMethod method: HTTPMethod,
+        success : ClosureSuccess?,
+        failure:ClosureError?,
+        internalheaders: HTTPHeaders? = nil
+        ) -> URLSessionTask? {
+        
+        var url: String = BASEURL
+        
+        // If api tag is there then append it with base url
+        if let `tag` = tag {
+            url = url + tag
+        }
+        
+        let uRequest = SessionManager.default.request(
+            url,
+            method: method,
+            parameters: parameters,
+            encoding: URLEncoding.default,
+            headers: internalheaders ?? headers
+        )
+        
+        self.handleResponseStatus(
+            uRequest: uRequest,
+            success: success,
+            failure: failure
+        )
+        
         return uRequest.task
     }
     
-    // Api Service without tag
-    func apiService(param parameters: [String: AnyObject]?,apiMethod method: HTTPMethod, success : ClosureSuccess?, failure:ClosureError?) -> URLSessionTask {
-        let uRequest = SessionManager.default.request(BASEURL, method: method, parameters: parameters, encoding: JSONEncoding.default, headers: headers)
-        self.handleResponseStatus(uRequest: uRequest, success: success, failure: failure)
-        return uRequest.task!
-    }
-    
     // Api Service for media
-    func POST(param parameters:[String: AnyObject]?, tag:String?, multipartFormData: @escaping (MultipartFormData) -> Void, success:ClosureSuccess?,  failure:ClosureError?) -> Void {
+    func POST(
+        param parameters:[String: AnyObject]?,
+        tag:String?,
+        multipartFormData: @escaping (MultipartFormData) -> Void,
+        success:ClosureSuccess?,
+        failure:ClosureError?
+        ) -> Void {
         
-        SessionManager.default.upload(multipartFormData: { (multipart) in
+        SessionManager.default.upload(multipartFormData: { [weak self] (multipart) in
+            
+            guard let _ = self, let `parameters` = parameters else {
+                return
+            }
+            
             multipartFormData(multipart)
             
-            if parameters != nil {
-                for (key, value) in parameters! {
-                    multipart.append("\(value)".data(using: .utf8)!, withName: key)
-                    //  multipart.append(value.data(using: String.Encoding.utf8.rawValue)! , withName: key)
-                }
+            for (key, value) in parameters {
+                multipart.append("\(value)".data(using: .utf8)!, withName: key)
             }
             
-        },  to: (BASEURL + (tag ?? "")), method: .post , headers: headers) { (encodingResult) in
+        },  to: (BASEURL + (tag ?? "")), method: .post , headers: headers) { [weak self] (encodingResult) in
+            
+            guard let `self` = self else {
+                return
+            }
+            
             switch encodingResult {
+                
             case .success(let uRequest, _, _):
-                self.handleResponseStatus(uRequest: uRequest, success: success, failure: failure)
-                break
+                
+                self.handleResponseStatus(
+                    uRequest: uRequest,
+                    success: success,
+                    failure: failure
+                )
+                
             case .failure(let encodingError):
-                print(encodingError)
+                print("encodingError ====>>>> \(encodingError)")
                 break
             }
+            
         }
     }
 }
 
+//MARK:- --------- Response Handler -
 extension Networking {
     
-    fileprivate func handleResponseStatus(uRequest:DataRequest , success : ClosureSuccess?, failure:ClosureError?) {
+    fileprivate func handleResponseStatus(
+        uRequest:DataRequest ,
+        success : ClosureSuccess?,
+        failure:ClosureError?
+        ) {
         
         self.logging(request: uRequest)
         
-        uRequest.responseJSON { (response) in
+        uRequest.responseJSON {[weak self] (response) in
+            
+            guard let `self` = self else {
+                return
+            }
             
             self.logging(response: response)
-            if(response.result.error == nil && ([200, 201, 401] .contains(response.response!.statusCode)) ) {
-                if(success != nil) {
-                    success!(uRequest.task!, response.result.value as AnyObject, response.data)
+            
+            if response.result.error == nil, let subResponse = response.response, self.arrSuccessStatus.contains(subResponse.statusCode) {
+                
+                guard let `success` = success, let task = uRequest.task else {
+                    return
                 }
+                
+                success(
+                    task,
+                    response.result.value as AnyObject,
+                    response.data
+                )
+                
             } else {
-                if(failure != nil) {
+                
+                guard let `failure` = failure, let task = uRequest.task else {
+                    return
+                }
+                
+                if response.result.error != nil {
                     
-                    if response.result.error != nil {
-                        failure!(uRequest.task!,nil, response.result.error as NSError?)
-                    }
-                    else {
-                        guard let dict = response.result.value as? [String : AnyObject], let statusCode = dict["status"] as? Int, let message = dict["message"] as? String else {
-                            return failure!(uRequest.task!,nil, nil)
-                        }
-                        
-                        let error = NSError(domain: "", code: statusCode, userInfo: dict)
-                        failure!(uRequest.task!, message, error)
+                    failure(
+                        task,
+                        nil,
+                        response.result.error as NSError?
+                    )
+                    
+                } else {
+                    
+                    guard let dict = response.result.value as? [String : AnyObject],
+                        let statusCode = dict["status"] as? Int,
+                        let message = dict["message"] as? String
+                        else {
+                            return failure(
+                                uRequest.task!,
+                                nil,
+                                nil
+                            )
                     }
                     
+                    let error = NSError(
+                        domain: "",
+                        code: statusCode,
+                        userInfo: dict
+                    )
+                    
+                    failure(
+                        task,
+                        message,
+                        error
+                    )
                 }
             }
         }
@@ -172,52 +263,52 @@ extension Networking {
      */
     
     /*
-    func checkResponseStatusAndShowAlert(showAlert:Bool, responseobject: AnyObject?, strApiTag:String) -> Bool {
-        
-        if let meta = responseobject?.value(forKey: CJsonMeta) as? [String : Any] {
-            
-            switch meta.valueForInt(key: CJsonStatus) {
-            case CStatusZero:
-                return true
-                
-            case CStatusFour:
-                return true
-                
-            case CStatusTen : //register from admin
-                return true
-                
-            case CStatus200 : //register from admin
-                return true
-                
-            default:
-                if showAlert {
-                    let message = meta.valueForString(key: CJsonMessage)
-                    GCDMainThread.async {
-                        CTopMostViewController.presentAlertViewWithOneButton(alertTitle: "", alertMessage: message, btnOneTitle: CBtnOk, btnOneTapped: nil)
-                    }
-                }
-            }
-        }else {
-            if let status = responseobject?.value(forKey: "status") as? Int{
-                if status == 401{
-                    let token = (CUserDefaults.value(forKey: UserDefaultDeviceToken)) as? String ?? ""
-                    if !token.isBlank{
-                        appDelegate.logOut()
-                    }
-                }
-            }
-        }
-        
-        return false
-    }
-    
-    func actionOnAPIFailure(errorMessage:String?, showAlert:Bool, strApiTag:String,error:NSError?) -> Void {
-        
-        if showAlert && errorMessage != nil {
-            CTopMostViewController.presentAlertViewWithOneButton(alertTitle: "", alertMessage: errorMessage!, btnOneTitle: CBtnOk, btnOneTapped: nil)
-        }
-        
-        print("API Error =" + "\(strApiTag )" + "\(String(describing: error?.localizedDescription))" )
-    }
-    */
+     func checkResponseStatusAndShowAlert(showAlert:Bool, responseobject: AnyObject?, strApiTag:String) -> Bool {
+     
+     if let meta = responseobject?.value(forKey: CJsonMeta) as? [String : Any] {
+     
+     switch meta.valueForInt(key: CJsonStatus) {
+     case CStatusZero:
+     return true
+     
+     case CStatusFour:
+     return true
+     
+     case CStatusTen : //register from admin
+     return true
+     
+     case CStatus200 : //register from admin
+     return true
+     
+     default:
+     if showAlert {
+     let message = meta.valueForString(key: CJsonMessage)
+     GCDMainThread.async {
+     CTopMostViewController.presentAlertViewWithOneButton(alertTitle: "", alertMessage: message, btnOneTitle: CBtnOk, btnOneTapped: nil)
+     }
+     }
+     }
+     }else {
+     if let status = responseobject?.value(forKey: "status") as? Int{
+     if status == 401{
+     let token = (CUserDefaults.value(forKey: UserDefaultDeviceToken)) as? String ?? ""
+     if !token.isBlank{
+     appDelegate.logOut()
+     }
+     }
+     }
+     }
+     
+     return false
+     }
+     
+     func actionOnAPIFailure(errorMessage:String?, showAlert:Bool, strApiTag:String,error:NSError?) -> Void {
+     
+     if showAlert && errorMessage != nil {
+     CTopMostViewController.presentAlertViewWithOneButton(alertTitle: "", alertMessage: errorMessage!, btnOneTitle: CBtnOk, btnOneTapped: nil)
+     }
+     
+     print("API Error =" + "\(strApiTag )" + "\(String(describing: error?.localizedDescription))" )
+     }
+     */
 }
